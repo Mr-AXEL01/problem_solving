2de7e8014fad54d52f8abd5888f1b365c438ec14
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-30 15:31:19 +0100

    instance an object from the Palindromic class to check if we solve the problem .

--- src/main/java/net/axel/ps/Main.java
+++ src/main/java/net/axel/ps/Main.java
@@ -2,8 +2,7 @@ package net.axel.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
+        Palindromic palindromic = new Palindromic();
+        palindromic.setPalindromic();
     }
 }
